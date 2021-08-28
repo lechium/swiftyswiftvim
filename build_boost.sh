083f4987dc8f3a16301f1232ada9ad76035f280a
kevinbradley <kevinbradley@nitombp.local>
2021-08-28 15:40:03 -0700

    make it build in newer macOS versions again

--- build_boost.sh
+++ build_boost.sh
@@ -11,12 +11,26 @@ SSVIM_BUILD_DIR=$DIR/build
 # Libs to /vendor/boost/lib
 BOOST_PREFIX=$SSVIM_BUILD_DIR/vendor/boost/
 BOOST_LIB_DIR=$SSVIM_BUILD_DIR/vendor/boost/lib/
+BOOST_LIB_ARRAY=("libboost_coroutine.a" "libboost_context.a" "libboost_filesystem.a" "libboost_program_options.a" "libboost_system.a" "libboost_thread.a")
+LIB_COUNT=6
 
-if [[ -d $BOOST_LIB_DIR ]] && [[ -z $CI ]]; then
-    echo "Boost probably is built, skipping"
-    exit 0
-fi
+boost_lib_check() {
+    if [ -d $BOOST_LIB_DIR ]; then
+        for LIB in ${BOOST_LIB_ARRAY[@]}
+        do
+            FILE="${BOOST_LIB_DIR}/${LIB}"
+            if [ -f $FILE ]; then
+                COUNT=$((COUNT + 1))
+            fi
+        done
+    fi
 
+    if [[ $LIB_COUNT == $COUNT ]] && [[ -z $CI ]]; then
+        echo "boost libraries already exist! exiting."
+        exit 0
+    fi
+}
+boost_lib_check
 mkdir -p $BOOST_LIB_DIR
 
 # Wants to build from here.
@@ -32,5 +46,5 @@ cd $DIR/vendor/boost
     --layout-tagged install \
     threading=multi \
     link=static \
-    cxxflags=-stdlib=libc++ linkflags=-stdlib=libc++ 
+    cxxflags="-std=c++14 -stdlib=libc++" linkflags="-stdlib=libc++ -std=c++14"
 
